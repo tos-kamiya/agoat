00845c3110c074804ef6cb40073b21556397f8ad
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-08-18 07:37:50 +0900

    add a test case for trees including various types

--- src/andor_tree.py
+++ src/andor_tree.py
@@ -6,27 +6,40 @@ ORDERED_AND = "&"
 ORDERED_OR = "|"
 
 def normalize_tree(node):
-    if not(isinstance(node, list) and node[0] in (ORDERED_AND, ORDERED_OR)):
+    if not(isinstance(node, list) and node and node[0] in (ORDERED_AND, ORDERED_OR)):
         return node
     t0 = node[0]
     assert t0 in (ORDERED_AND, ORDERED_OR)
-    t = [t0]
-    for item in node[1:]:
-        oi = normalize_tree(item)
-        if oi == [ORDERED_OR]:
-            if t0 == ORDERED_AND:
-                t = oi[:]
-                break  # for item
-            else:
-                continue  # for item
-        if oi[0] == t0:
-            t.extend(oi[1:])
-        else:
-            t.append(oi)
     if t0 == ORDERED_OR:
-        st = [t0]
-        st.extend(sort_uniq(t[1:]))
-        t = st
+        items = []
+        nodes = []
+        for item in node[1:]:
+            oi = normalize_tree(item)
+            if oi == [ORDERED_OR]:
+                continue  # for item
+            if isinstance(oi, list) and oi:
+                oi0 = oi[0]
+                if oi0 == ORDERED_OR:
+                    nodes.extend(oi[1:])
+                elif oi0 == ORDERED_AND:
+                    nodes.append(oi)
+                else:
+                    items.append(oi)
+            else:
+                items.append(oi)
+        t = [t0]
+        t.extend(sort_uniq(items))
+        t.extend(sort_uniq(nodes))
+    else:
+        t = [t0]
+        for item in node[1:]:
+            oi = normalize_tree(item)
+            if oi == [ORDERED_OR]:
+                return oi[:]
+            if isinstance(oi, list) and oi and oi[0] == ORDERED_AND:
+                t.extend(oi[1:])
+            else:
+                t.append(oi)
     if len(t) == 2:
         return t[1]
     return t
--- test/andor_tree_test.py
+++ test/andor_tree_test.py
@@ -8,29 +8,31 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'
 
 import andor_tree as at
 
+SameAsInput = object()
+
 class AndOrTreeTest(unittest.TestCase):
-    def normalize_testing(self, input_tree, expected):
+    def normalize_testing(self, input_tree, expected=SameAsInput):
         tree = input_tree[:]
         normalized = at.normalize_tree(tree[:])
         assert tree == input_tree
-        if expected is None:
+        if expected is SameAsInput:
             expected = input_tree
         self.assertEqual(normalized, expected)
 
     def test_normalize_tree_len0(self):
-        self.normalize_testing([at.ORDERED_AND], None)
-        self.normalize_testing([at.ORDERED_OR], None)
+        self.normalize_testing([at.ORDERED_AND], SameAsInput)
+        self.normalize_testing([at.ORDERED_OR], SameAsInput)
 
     def test_normalize_tree_len1(self):
         self.normalize_testing([at.ORDERED_AND, 'a'], 'a')
         self.normalize_testing([at.ORDERED_OR, 'a'], 'a')
 
     def test_normalize_tree_len2(self):
-        self.normalize_testing([at.ORDERED_AND, 'b', 'a'], None)
+        self.normalize_testing([at.ORDERED_AND, 'b', 'a'], SameAsInput)
         self.normalize_testing([at.ORDERED_OR, 'b', 'a'], [at.ORDERED_OR, 'a', 'b'])
 
     def test_normalize_tree_len2_dup(self):
-        self.normalize_testing([at.ORDERED_AND, 'a', 'a'], None)
+        self.normalize_testing([at.ORDERED_AND, 'a', 'a'], SameAsInput)
         self.normalize_testing([at.ORDERED_OR, 'a', 'a'], 'a')
 
     def test_normalize_tree_dep2_len0_len1(self):
@@ -48,8 +50,15 @@ class AndOrTreeTest(unittest.TestCase):
     def test_normalize_tree_dep2_len1_len0(self):
         self.normalize_testing([at.ORDERED_AND, 'a', [at.ORDERED_AND]], 'a')
         self.normalize_testing([at.ORDERED_AND, 'a', [at.ORDERED_OR]], [at.ORDERED_OR])
-        self.normalize_testing([at.ORDERED_OR, 'a', [at.ORDERED_AND]], None)
+        self.normalize_testing([at.ORDERED_OR, 'a', [at.ORDERED_AND]], SameAsInput)
         self.normalize_testing([at.ORDERED_OR, 'a', [at.ORDERED_OR]], 'a')
+
+    def test_normalize_tree_of_various_item_type(self):
+        for item in ('a', 1, None, (), [], (1,), [1]):
+            self.normalize_testing([at.ORDERED_AND, item, [at.ORDERED_AND]], item)
+            self.normalize_testing([at.ORDERED_AND, item, [at.ORDERED_OR]], [at.ORDERED_OR])
+            self.normalize_testing([at.ORDERED_OR, item, [at.ORDERED_AND]], SameAsInput)
+            self.normalize_testing([at.ORDERED_OR, item, [at.ORDERED_OR]], item)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
