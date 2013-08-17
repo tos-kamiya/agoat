1d06ed97cd1f57c0f8a0bb86aeb52036b8d0a4cd
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-08-18 06:53:30 +0900

    refactor with introducing helper function

--- test/andor_tree_test.py
+++ test/andor_tree_test.py
@@ -9,94 +9,47 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'
 import andor_tree as at
 
 class AndOrTreeTest(unittest.TestCase):
-    def test_normalize_tree_len0(self):
-        tree = [at.ORDERED_AND]
-        expected = tree[:]
-        nt = at.normalize_tree(tree)
-        self.assertSequenceEqual(nt, expected)
+    def normalize_testing(self, input_tree, expected):
+        tree = input_tree[:]
+        normalized = at.normalize_tree(tree[:])
+        assert tree == input_tree
+        if expected is None:
+            expected = input_tree
+        self.assertEqual(normalized, expected)
 
-        tree = [at.ORDERED_OR]
-        expected = tree[:]
-        nt = at.normalize_tree(tree)
-        self.assertSequenceEqual(nt, expected)
+    def test_normalize_tree_len0(self):
+        self.normalize_testing([at.ORDERED_AND], None)
+        self.normalize_testing([at.ORDERED_OR], None)
 
     def test_normalize_tree_len1(self):
-        tree = [at.ORDERED_AND, 'a']
-        expected = 'a'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, 'a']
-        expected = 'a'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
+        self.normalize_testing([at.ORDERED_AND, 'a'], 'a')
+        self.normalize_testing([at.ORDERED_OR, 'a'], 'a')
 
     def test_normalize_tree_len2(self):
-        tree = [at.ORDERED_AND, 'b', 'a']
-        expected = tree[:]
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, 'b', 'a']
-        expected = [at.ORDERED_OR, 'a', 'b']
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
+        self.normalize_testing([at.ORDERED_AND, 'b', 'a'], None)
+        self.normalize_testing([at.ORDERED_OR, 'b', 'a'], [at.ORDERED_OR, 'a', 'b'])
 
     def test_normalize_tree_len2_dup(self):
-        tree = [at.ORDERED_AND, 'a', 'a']
-        expected = tree[:]
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, 'a', 'a']
-        expected = 'a'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
+        self.normalize_testing([at.ORDERED_AND, 'a', 'a'], None)
+        self.normalize_testing([at.ORDERED_OR, 'a', 'a'], 'a')
 
     def test_normalize_tree_dep2_len0_len1(self):
-        tree = [at.ORDERED_AND, [at.ORDERED_AND, 'b']]
-        expected = 'b'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, [at.ORDERED_OR, 'b']]
-        expected = 'b'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_AND, [at.ORDERED_OR, 'b']]
-        expected = 'b'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, [at.ORDERED_AND, 'b']]
-        expected = 'b'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
+        self.normalize_testing([at.ORDERED_AND, [at.ORDERED_AND, 'b']], 'b')
+        self.normalize_testing([at.ORDERED_AND, [at.ORDERED_OR, 'b']], 'b')
+        self.normalize_testing([at.ORDERED_OR, [at.ORDERED_AND, 'b']], 'b')
+        self.normalize_testing([at.ORDERED_OR, [at.ORDERED_OR, 'b']], 'b')
 
     def test_normalize_tree_dep2_len1_len1(self):
-        tree = [at.ORDERED_AND, 'a', [at.ORDERED_AND, 'b']]
-        expected = [at.ORDERED_AND, 'a', 'b']
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, 'a', [at.ORDERED_OR, 'b']]
-        expected = [at.ORDERED_OR, 'a', 'b']
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_AND, 'a', [at.ORDERED_OR, 'b']]
-        expected = [at.ORDERED_AND, 'a', 'b']
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, 'a', [at.ORDERED_AND, 'b']]
-        expected = [at.ORDERED_OR, 'a', 'b']
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
+        self.normalize_testing([at.ORDERED_AND, 'a', [at.ORDERED_AND, 'b']], [at.ORDERED_AND, 'a', 'b'])
+        self.normalize_testing([at.ORDERED_AND, 'a', [at.ORDERED_OR, 'b']], [at.ORDERED_AND, 'a', 'b'])
+        self.normalize_testing([at.ORDERED_OR, 'a', [at.ORDERED_AND, 'b']], [at.ORDERED_OR, 'a', 'b'])
+        self.normalize_testing([at.ORDERED_OR, 'a', [at.ORDERED_OR, 'b']], [at.ORDERED_OR, 'a', 'b'])
 
     def test_normalize_tree_dep2_len1_len0(self):
-        tree = [at.ORDERED_AND, 'a', [at.ORDERED_AND]]
-        expected = 'a'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, 'a', [at.ORDERED_OR]]
-        expected = 'a'
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_AND, 'a', [at.ORDERED_OR]]
-        expected = [at.ORDERED_OR]
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
-
-        tree = [at.ORDERED_OR, 'a', [at.ORDERED_AND]]
-        expected = [at.ORDERED_OR, [at.ORDERED_AND], 'a']
-        self.assertSequenceEqual(at.normalize_tree(tree), expected)
+        self.normalize_testing([at.ORDERED_AND, 'a', [at.ORDERED_AND]], 'a')
+        self.normalize_testing([at.ORDERED_AND, 'a', [at.ORDERED_OR]], [at.ORDERED_OR])
+        self.normalize_testing([at.ORDERED_OR, 'a', [at.ORDERED_AND]], None)
+        self.normalize_testing([at.ORDERED_OR, 'a', [at.ORDERED_OR]], 'a')
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
