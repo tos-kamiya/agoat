734e547acf703eeed62d382082f4f8994baf29ae
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-09-06 12:50:32 +0900

    modify c command

--- src/agoat.py
+++ src/agoat.py
@@ -32,32 +32,22 @@ def list_methods(soot_dir, output_file):
             out.write("%s\t%s\n" % (clz, msig))
 
 
-def generate_call_tree(entry_point, soot_dir, output_file, pretty_print=False):
+def generate_call_tree_and_node_summary(entry_point, soot_dir, output_file, pretty_print=False):
     class_table = dict((clz, cd) \
             for clz, cd in jp.read_class_table_from_dir_iter(soot_dir))
     class_table = cb.inss_to_tree_in_class_table(class_table)
     call_tree = cb.extract_call_andor_tree(class_table, entry_point)
-
-    with open_w_default(output_file, "wb", sys.stdout) as out:
-        if pretty_print:
-            pp = pprint.PrettyPrinter(indent=4, stream=out)
-            pp.pprint(call_tree)
-        else:
-            pickle.dump(call_tree, out)
-
-
-def generate_node_summary(call_tree_file, output_file, pretty_print=False):
-    with open_w_default(call_tree_file, "rb", sys.stdin) as inp:
-        call_tree = pickle.load(inp)
-
     node_summary_table = ns.extract_node_summerize_table(call_tree)
 
     with open_w_default(output_file, "wb", sys.stdout) as out:
         if pretty_print:
             pp = pprint.PrettyPrinter(indent=4, stream=out)
+            out.write("call tree:\n")
+            pp.pprint(call_tree)
+            out.write("node summary table:\n")
             pp.pprint(node_summary_table)
         else:
-            pickle.dump(node_summary_table, out)
+            pickle.dump((call_tree, node_summary_table), out)
 
 
 def main(argv):
@@ -73,20 +63,21 @@ def main(argv):
     psr_ep.add_argument('-s', '--soot-dir', action='store', help='soot directory', default='sootOutput')
     psr_ep.add_argument('-o', '--output', action='store', help="output file. '-' for standard output", default='-')
 
-    psr_mt = subpsrs.add_parser('m', help='listing methods')
+    psr_mt = subpsrs.add_parser('m', help='listing methods (defined methods and used ones)')
     psr_mt.add_argument('-s', '--soot-dir', action='store', help='soot directory', default='sootOutput')
     psr_mt.add_argument('-o', '--output', action='store', help="output file. '-' for standard output", default='-')
 
-    psr_ct = subpsrs.add_parser('c', help='generate call tree')
+    psr_ct = subpsrs.add_parser('c', help='generate call tree and node summary table')
     psr_ct.add_argument('entrypoint', action='store', help='entry-point class')
     psr_ct.add_argument('-s', '--soot-dir', action='store', help='soot directory', default='sootOutput')
     psr_ct.add_argument('-o', '--output', action='store', help="output file. '-' for standard output", default='-')
     psr_ct.add_argument('-p', '--pretty-print', action='store_true', help='print call-tree in human-readable format')
 
-    psr_ns = subpsrs.add_parser('n', help='generate node summary')
-    psr_ns.add_argument('calltree', action='store', help="call-tree file. '-' for standard input")
-    psr_ns.add_argument('-o', '--output', action='store', help="output file. '-' for standard output", default='-')
-    psr_ns.add_argument('-p', '--pretty-print', action='store_true', help='print call-tree in human-readable format')
+    psr_sh = subpsrs.add_parser('s', help='search')
+    psr_sh.add_argument('calltree', action='store', help="call-tree file. '-' for standard input")
+    psr_sh.add_argument('query', action='store', nargs='+', help="query words")
+    psr_sh.add_argument('-o', '--output', action='store', help="output file. '-' for standard output", default='-')
+    psr_sh.add_argument('-p', '--pretty-print', action='store_true', help='print call-tree in human-readable format')
 
     args = psr.parse_args(argv[1:])
     if args.command == 'e':
@@ -96,9 +87,7 @@ def main(argv):
     elif args.command == 'c':
         entry_point_msig = jp.MethodSig(None, "main", ("java.lang.String[]",))
         entry_point = (args.entrypoint, entry_point_msig)
-        generate_call_tree(entry_point, args.soot_dir, args.output, args.pretty_print)
-    elif args.command == 'n':
-        generate_node_summary(args.calltree, args.output, args.pretty_print)
+        generate_call_tree_and_node_summary(entry_point, args.soot_dir, args.output, args.pretty_print)
     else:
         assert False
 
