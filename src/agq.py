3e4ad62756ad8d2a512fb7e2ccaa1990310229e3
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-09-20 00:20:45 +0900

    remove unused, not-working option

--- src/agq.py
+++ src/agq.py
@@ -228,8 +228,6 @@ def main(argv):
             help="max depth of subtree. -1 for unlimited depth. (default '%d')" % _c.defalut_max_depth_of_subtree,
             default=_c.defalut_max_depth_of_subtree)
     psr_q.add_argument('-f', '--output-form', choices=('callnode', 'treecut', 'path'), default='path')
-    psr_q.add_argument('-N', '--node', action='store_true',
-            help="show and-or-call tree node w/o expanding it to paths")
     color_choices=('always', 'never', 'auto')
     psr_q.add_argument('--color', '--colour', action='store', choices=color_choices, dest='color', 
             help="hilighting with ANSI color.",
